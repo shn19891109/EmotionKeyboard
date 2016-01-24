7808727319c02baca01984bb74d03dde61c2cb1d
shn <1058216614@qq.com>
2016-01-24 21:06:58 +0800

    添加文件

---
+++ 表情键盘/HMTextView.h
@@ -0,0 +1,13 @@
+//
+//  HMTextView.h
+//  表情键盘
+//
+//  Created by 苏浩楠 on 16/1/24.
+//  Copyright © 2016年 苏浩楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMTextView : UITextView
+
+@end
