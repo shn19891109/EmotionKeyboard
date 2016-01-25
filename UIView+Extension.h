ed95037991ea6b858a9983fcdc92a0a200833df5
shn <1058216614@qq.com>
2016-01-25 10:00:52 +0800

    添加分类

---
+++ UIView+Extension.h
@@ -0,0 +1,23 @@
+//
+//  UIView+Extension.h
+//
+//
+//  Created by zjx on 14-6-27.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Extension)
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat maxX;
+@property (nonatomic, assign) CGFloat minX;
+@property (nonatomic, assign) CGFloat maxY;
+@property (nonatomic, assign) CGFloat minY;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@property (nonatomic, assign) CGSize size;
+@end
